844c52918780458f4a3b7beaa9b5605a4705511e
Mazilu Mircea <mmirceaiulius@gmail.com>
2026-01-04 13:32:08 +0200

    Initial Commit

---
+++ Pixel Blast Arena/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pixel Blast Arena
+//
+//  Created by Mircea Mazilu on 04.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pixel Blast Arena/Pixel_Blast_ArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pixel_Blast_ArenaApp.swift
+//  Pixel Blast Arena
+//
+//  Created by Mircea Mazilu on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Pixel_Blast_ArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
